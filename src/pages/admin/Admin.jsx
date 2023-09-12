28115051d8b363953aec3159380d2710659fd219
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-09-12 13:57:33 +0200

    upd

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import "./Admin.scss";
-
-import Footer from "../../components/Footer";
-import Sidebar from "../../components/Sidebar";
+import Layout from "../../components/Layout";
 
 const Admin = () => {
   return (
-    <>
+    <Layout>
       <div className="admin">
-        <div className="admin__content">
-          <img
-            src="https://freesvg.org/img/1667812423coffee-shop-logo-concept.png"
-            alt="coffe_logo"
-          />
-          <h1>Üdvözöljük</h1>
-          <h2>KunPao's Coffee POS</h2>
-        </div>
-        <Footer />
-        <Sidebar />
+        <img
+          src="https://freesvg.org/img/1667812423coffee-shop-logo-concept.png"
+          alt="coffe_logo"
+        />
+        <h1>Üdvözöljük</h1>
+        <h2>KunPao's Coffee Management</h2>
       </div>
-    </>
+    </Layout>
   );
 };
 
