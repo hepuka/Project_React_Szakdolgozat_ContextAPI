77b432a1c08825902d9561f180e0abdd2d731f15
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-09-15 12:06:28 +0200

    upd

--- src/pages/employees/Placeorder.jsx
+++ src/pages/employees/Placeorder.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Placeorder.scss";
 import Layout from "../../components/Layout";
+import { useParams } from "react-router-dom";
 
 const Placeorder = () => {
+  const { id } = useParams();
   return (
     <Layout>
       <h1>PLACE ORDER</h1>
+      <h1>{id}</h1>
     </Layout>
   );
 };
