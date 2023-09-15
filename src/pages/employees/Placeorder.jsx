8ea68d229d411824a8695c6237296f44d947bd8e
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-09-15 13:42:59 +0200

    upd

--- src/pages/employees/Placeorder.jsx
+++ src/pages/employees/Placeorder.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./Placeorder.scss";
 import Layout from "../../components/Layout";
 import { useParams } from "react-router-dom";
+import { useStateValue } from "../../ContextAPI/StateProvider";
 
 const Placeorder = () => {
   const { id } = useParams();
+  const [{ user, userName }] = useStateValue();
   return (
     <Layout>
       <div className="placeorder">
         <div className="placeorder__card placeorder__tablenumber">
           <h1>{id}. asztal</h1>
+          <h2>{userName}</h2>
         </div>
         <div className="placeorder__card placeorder__tablebuttons"> </div>
         <div className="placeorder__card placeorder__tableproducts"> </div>
