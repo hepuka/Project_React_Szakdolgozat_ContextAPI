d392c81ff4ed093a348d77c099ae64f40c1700d0
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-09-15 14:00:39 +0200

    upd

--- src/pages/employees/Placeorder.jsx
+++ src/pages/employees/Placeorder.jsx
@@ -3,10 +3,18 @@ import "./Placeorder.scss";
 import Layout from "../../components/Layout";
 import { useParams } from "react-router-dom";
 import { useStateValue } from "../../ContextAPI/StateProvider";
+import { NavLink } from "react-router-dom";
 
 const Placeorder = () => {
   const { id } = useParams();
   const [{ user, userName }] = useStateValue();
+
+  const activeLink = ({ isActive }) => {
+    return isActive
+      ? "sidebar__button sidebar__button_active"
+      : "sidebar__button";
+  };
+
   return (
     <Layout>
       <div className="placeorder">
@@ -14,7 +22,38 @@ const Placeorder = () => {
           <h1>{id}. asztal</h1>
           <h2>{userName}</h2>
         </div>
-        <div className="placeorder__card placeorder__tablebuttons"> </div>
+
+        <div className="placeorder__card placeorder__tablebuttons">
+          <div className="sidebar__buttons">
+            <NavLink to="/" className={activeLink}>
+              Összes
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Ital
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Gyümölcslevek
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Kávé
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Tea
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Sütemények
+            </NavLink>
+
+            <NavLink to="/" className={activeLink}>
+              Péksütemények
+            </NavLink>
+          </div>
+        </div>
         <div className="placeorder__card placeorder__tableproducts"> </div>
         <div className="placeorder__card placeorder__tableorders"> </div>
         <div className="placeorder__card placeorder__tablepayment"> </div>
